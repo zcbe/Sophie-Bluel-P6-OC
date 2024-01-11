0aebf7c456db260baae7fcc6dde7252a8148c51d
zcbe <151560157+zcbe@users.noreply.github.com>
2024-01-12 00:03:02 +0100

    add code js for add new projet
    Étape 3.3 : Envoi d’un nouveau projet au back-end via le
    formulaire de la modale
    
    Étape 3.4 : Traitement de la réponse de l’API pour afficher
    dynamiquement la nouvelle image de la modale.

--- FrontEnd/assets/js/modal.js
+++ FrontEnd/assets/js/modal.js
@@ -41,7 +41,7 @@
     };
 
 // Récupération des travaux pour la modale 
-async function getWorksModal() {
+function getWorksModal() {
     fetch("http://localhost:5678/api/works")
     .then(response => response.json())
     .then(dataWorksModal => {
@@ -110,4 +110,163 @@ async function getWorksModal() {
         });
 });     
 };
+
+
+
+// Formulaire d'envoi d'un nouveau projet
+    // Eléments requis pour valider l'ajout d'un projet
+    const formModal = document.getElementById("modal__two-form");
+    const inputImage = document.getElementById("addPhoto");
+    const titleProject = document.getElementById("photoTitle");
+    const categoryProject = document.getElementById("photoCategories");
+    const validateProject = document.getElementById("validateProject");
+    // Message d'erreur
+    let errorForm = document.getElementById("errorForm");
+    // Prévisualisation d'une photo
+    inputImage.addEventListener("change", previewPicture);
+    function previewPicture(event) {
+        event.preventDefault();
+        // Utilisation de l'objet FileReader pour lire l'image
+        const reader = new FileReader();
+        reader.readAsDataURL(inputImage.files[0]);
+        // Listener de chargement sur la lecture de l'image
+        reader.addEventListener("load", () => {
+            previewImage.src = reader.result;
+        });
+        
+        // Affichage de l'image 
+        const pictureContainer = document.querySelector(".modal__two-imgcontainer");
+        const previewImage = document.createElement("img");
+        // On ajoute un id pour pouvoir la supprimer lors du reset du formulaire
+        previewImage.setAttribute("id", "previewImage");
+        // On relie l'image au parent Containeur
+        pictureContainer.appendChild(previewImage);
+        // On lui donne les dimensions pour l'affichage
+        previewImage.style.width = "150px";
+        previewImage.style.height = "183px";
+        // On cache le label qui dépasse lors de prévisualisation de la photo
+        const labelPicture = document.querySelector(".modal__two-textAddPhoto");
+        labelPicture.style.opacity = "0";
+    };
+
+
+
+
+
+        
+    // Reset formulaire
+    function resetForm() {
+        document.getElementById("modal__two-form").reset();
+        // Suppresion de la prévisualisation de la photo du projet après validation formulaire
+        const pictureContainer = document.querySelector(".modal__two-imgcontainer");
+        const previewImage = document.getElementById("previewImage");
+        if (previewImage) {
+            pictureContainer.removeChild(previewImage);
+        };
+        // On affiche de nouveau le label
+        const labelPicture = document.querySelector(".modal__two-textAddPhoto");
+        labelPicture.style.opacity = "1";
+    };
+    // Ajout des catégories au formulaire d'ajout de projet 
+    fetch("http://localhost:5678/api/categories")
+        .then(response => response.json())
+        .then(dataCategories => {
+            // On récupère le select pour ajouter les catégories
+            const select = document.getElementById("photoCategories");
+            // Catégorie vide pour le visuel
+            const emptyOption = document.createElement('option');
+            select.appendChild(emptyOption);
+            // Récupération dynamique des catégories présentes sur API
+            dataCategories.forEach((category) => {
+                const option = document.createElement('option');
+                option.innerText = category.name;
+                option.value = category.id;
+                select.appendChild(option);
+            });
+        });
+    // Listeners sur les infos à soumettre pour que le bouton "Valider" passe au vert
+    inputImage.addEventListener("input", verifForm);
+    titleProject.addEventListener("input", verifForm);
+    categoryProject.addEventListener("input", verifForm);
+    
+    // Fonction de vérification si les conditions sont remplies = bouton "Valider" passe au vert
+    function verifForm() {
+        const minLengthTitle =3;
+        if (titleProject.value.length >=minLengthTitle && categoryProject.value !== "" && inputImage.value !== ""){
+            validateProject.classList.toggle("active");
+            errorForm.style.display = 'none';
+        } else {
+            errorForm.innerText = "Veuillez renseigner tous les champs";
+        }
+    };
+    // Fonction pour valider le formulaire
+    async function validationFormModal () {
+        // Sélection des infos pour soumettre le formulaire
+        const inputImageUrl = document.getElementById("addPhoto").files[0];
+        const titleProject = document.getElementById("photoTitle").value;
+        const categoryProject = document.getElementById("photoCategories").value;
+        // Sélection des galeries et de la modale
+        const gallery = document.querySelector(".gallery");
+        const galleryModal = document.querySelector(".modal__one-gallery");
+        const modalContainer = document.querySelector(".modal__container");
+        
+        // Test de récupération des infos
+           console.log(inputImageUrl);
+           console.log(titleProject);
+           console.table(categoryProject);
+        
+        // On crée le formulaire de soumission du projet
+        let formData = new FormData();
+        formData.append("image", inputImageUrl);
+        formData.append("title", titleProject);
+        formData.append("category", categoryProject);
+        /* Test de vérification que le formulaire est bien créé
+        console.log(formData);
+        */
+        const myToken = localStorage.getItem("token");
+        /* Test de récupération du token d'authentification pour soumettre nouveau projet
+        console.log(myToken);
+        */    
+            await fetch("http://localhost:5678/api/works", {
+                method: 'POST',
+                headers: {
+                    'Authorization': `Bearer ${myToken}`,
+                },
+                body: formData,
+            })
+            // Si la réponse est OK (status 200)
+            .then((response) => {
+                if (response.ok) {
+                    return response.json();
+                } 
+                throw new Error("Erreur lors du transfert");
+            })
+            .then((data) => {
+                // Réinitialisation des galeries
+                gallery.innerHTML = "";
+                galleryModal.innerHTML = "";
+                // On recharge dynamiquement les galeries
+                getWorks();
+                getWorksModal();
+                // Bouton "Valider" du formulaire redevient gris
+                validateProject.classList.remove("active");
+                // Fermeture de la modale
+                modalContainer.classList.remove("active");
+            })
+            .catch((error) => {
+                console.log(error);
+            });
+    };    
+    // Evènement au clic pour soumettre le formulaire
+    formModal.addEventListener("submit", (event) => {
+        event.preventDefault();
+        validationFormModal();
+        resetForm();
+        /* Vérification que le formulaire est bien remis à zéro et qu'il ne contient aucune donnée
+           console.log(inputImage.files)
+           console.log(titleProject.value)
+           console.log(categoryProject.value)
+        */
+    });
+
 
